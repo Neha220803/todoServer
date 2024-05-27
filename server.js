45b067b3f6217fd62da8d8691b1c901b1c72cbd6
Neeharika Srini <neeharika.srinivasan@gmail.com>
2024-05-27 11:30:30 +0530

    removed ports

--- server.js
+++ server.js
@@ -4,22 +4,21 @@ const cors = require('cors');
 const { v4: uuidv4 } = require('uuid');
 
 const app = express();
-const port =  3001;
 
 app.use(cors());
 app.use(bodyParser.json());
 
-let todos = []; 
+let todos = [];
 
 // Get all todos
-app.get('/tasks', (req, res) => {
+app.get('/api/tasks', (req, res) => {
     res.json(todos);
 });
 
 // Add a new todo
-app.post('/tasks', (req, res) => {
+app.post('/api/tasks', (req, res) => {
     const newTodo = {
-        id: uuidv4(), 
+        id: uuidv4(),
         ...req.body
     };
     todos.push(newTodo);
@@ -27,7 +26,7 @@ app.post('/tasks', (req, res) => {
 });
 
 // Edit an existing todo
-app.put('/tasks/:id', (req, res) => {
+app.put('/api/tasks/:id', (req, res) => {
     const { id } = req.params;
     const index = todos.findIndex(todo => todo.id === id);
     if (index !== -1) {
@@ -39,7 +38,7 @@ app.put('/tasks/:id', (req, res) => {
 });
 
 // Delete a todo
-app.delete('/tasks/:id', (req, res) => {
+app.delete('/api/tasks/:id', (req, res) => {
     const { id } = req.params;
     const index = todos.findIndex(todo => todo.id === id);
     if (index !== -1) {
@@ -50,6 +49,6 @@ app.delete('/tasks/:id', (req, res) => {
     }
 });
 
-app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
+app.listen(process.env.PORT, () => {
+    console.log('Server is running');
 });
